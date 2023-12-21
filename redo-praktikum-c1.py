afe47ec5c70623ae37cecf6f79ab9ae0773a0483
Vethzz <144196438+Vethzz@users.noreply.github.com>
2023-12-21 15:50:06 +0800

    add file c1

---
+++ redo-praktikum-c1.py
@@ -0,0 +1,60 @@
+print("praktikum-1")
+print()
+print("Nama Lenkap  : Redo Triansyah")
+print("Nim          : 231031094")
+print("Prodi        : Sistem Informasi C")
+      
+angkatan=2023
+print("angkatan adalah",angkatan)
+
+a=20
+print("Data adalah = ",a)
+print("Tipe Data adalah = ",type(a))
+
+print()
+b= 20.0
+print("Data Adalah =",b)
+print("Tipe Data adalah =",type(b))
+
+kampus= "Insitut Teknologi BJ Habiebie"
+print("Data Adalah =",kampus)
+print("Tipe Data adalah =",type(kampus))
+
+print()
+log = True
+print("Data Adalah =",log)
+print("Tipe Data adalah =",type(log))
+
+print()
+c = complex(3,4)
+print("Data Adalah =",c)
+print("Tipe Data adalah =",type(c))
+
+p=20
+q=5
+hasil=p+q
+print("hasil" ,p, "+",q,"=" ,hasil)
+print()
+p=20
+q=5
+hasil=p-q
+print("hasil" ,p, "-",q,"=" ,hasil)
+print()
+hasil = p * q
+print("Hasil",p,"*",q,"*",hasil)
+
+print()
+hasil = p / q
+print("Hasil",p,"/",q,"=",hasil)
+
+print()
+hasil = p % q
+print("Hasil",p,"%",q,"=",hasil)
+
+print()
+hasil = p // q
+print("Hasil",p,"//",q,"=",hasil)
+
+print()
+hasil = p ** q
+print("Hasil",p,"**",q,"*",hasil)
